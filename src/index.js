0ca2e53ed1de63875858e0c22e893a5b40091b5e
Katie <adlerk8@gmail.com>
2020-11-12 14:05:17 -0700

    working on front end

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <HashRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HashRouter>,
+  <Provider store={store}> 
+    <HashRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </HashRouter>
+    </Provider> ,
   document.getElementById('root')
 );
 
